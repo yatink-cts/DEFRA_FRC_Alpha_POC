37c7f94a2f797ba1cc0627fcf117233bc7afcfb2
SruthiS-CTS <Sruthi.Sundararajan@cognizant.com>
2023-05-05 16:10:30 +0100

    CommodityVolumeErrorHandling

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,55 @@
 module.exports = {
 
-  // Insert values here
+  "commodities": [
+    {
+      "text": "Palm oil",
+      "value": "palmoil",
+
+    },
+    {
+      "text": "Soya",
+      "value": "soya",
+
+    },
+    {
+      "text": "Cocoa",
+      "value": "cocoa",
+
+    },
+    {
+      "text": "Beef",
+      "value": "beef",
+
+    },
+    {
+      "text": "Coffee",
+      "value": "coffee",
+
+    },
+    {
+      "text": "Leather",
+      "value": "leather",
+
+    },
+    {
+      "text": "Maize",
+      "value": "maize",
+
+    },
+    {
+      "text": "Rubber",
+      "value": "rubber",
+
+    }
+  ],
+  errors: {
+
+    commodities: {
+
+      invalid: 'The volume must be less than 200 tonnes'
+
+    }
+  }
+
 
 }
--- app/routes.js
+++ app/routes.js
@@ -12,194 +12,196 @@ const router4 = govukPrototypeKit.requests.setupRouter()
 const router5 = govukPrototypeKit.requests.setupRouter()
 const router6 = govukPrototypeKit.requests.setupRouter()
 const router7 = govukPrototypeKit.requests.setupRouter()
-let food = {
-  'ketchup': '5 tbsp',
-  'mustard': '1 tbsp',
-  'pickle': '0 tbsp'
-};
-
+const router8 = govukPrototypeKit.requests.setupRouter()
 // Run this code when a form is submitted to 'juggling-balls-answer'
 router.post('/Login-answer', function (req, res) {
 
-    
-    var Accountholder = req.session.data['Accountholder'];
-    console.log("Accountholder:",Accountholder);
-  
-    // Check whether the variable matches a condition
-    if (Accountholder == "Yes"){
-      // Send user to next page
-      res.redirect('/signin')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/create-account')
-    }
-  
-  },
+
+  var Accountholder = req.session.data['Accountholder'];
+  console.log("Accountholder:", Accountholder);
+
+  // Check whether the variable matches a condition
+  if (Accountholder == "Yes") {
+    // Send user to next page
+    res.redirect('/signin')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/create-account')
+  }
+
+},
 
   router1.post('/FRC', function (req, res) {
 
-    let FRC=new Array();
-    
-   FRC = req.session.data['FRC'];
-   let len=FRC.length;
+    let FRC = new Array();
 
-   
-    console.log("FRC:",FRC);
-  
+    FRC = req.session.data['FRC'];
+    let len = FRC.length;
+
+
+   // console.log("FRC:", FRC);
+
     // Check whether the variable matches a condition
-    if (FRC == "None of the above"){
+    if (FRC == "None of the above") {
       // Send user to next page
       res.redirect('/ConfirmFRC')
     } else {
       // Send user to ineligible page
       res.redirect('/Page_3')
     }
-  
+
   },
-  
-  
-  router2.post('/AnnualIncome', function (req, res) {
+  router1.get('/CommodityVolumeGetController', function (req, res) {
 
+
+ //console.log("commoditydata",req.session.data['commodities']);
     
-    var AnnualIncome = req.session.data['AnnualIncome'];
-    if(AnnualIncome<500)
-    {
-    console.log("AnnualIncome:",AnnualIncome);
-    res.redirect('/Page_4');
-  }
-  else if(AnnualIncome>=500)
-  {
-    res.redirect('/CommodityVolumePage');
-  }
-  
-   
-  
-  },
-  router4.post('/ParentCompany', function (req, res) {
+ req.session.data['selectedCommodities']= req.session.data['FRC'].map(commodity=>{
 
-    
-    var ParentCompany = req.session.data['ParentCompany'];
-    var AnnualIncome = req.session.data['AnnualIncome'];
-    console.log("ParentCompany:",ParentCompany);
-  
-    // Check whether the variable matches a condition
-    if (ParentCompany =="No" ){
-     
-
-      if(AnnualIncome<500)
-      {  res.redirect('/checkanswers')}
-      // Send user to next page
-     
-    } else if(ParentCompany =="Yes" ) {
-      // Send user to ineligible page
-      res.redirect('/ParentCompanyDetails')
-    }
-  
-  },
-  router5.post('/ParentCompanyAnnualIncome', function (req, res) {
-
-    
-    var ParentCompanyAnnualIncome = req.session.data['ParentCompanyAnnualIncome'];
-   
-    console.log("ParentCompanyAnnualIncome:",ParentCompanyAnnualIncome);
-  
-    // Check whether the variable matches a condition
-    
-    
-      if(ParentCompanyAnnualIncome<500)
-
-      { 
-        res.redirect('/checkanswers1')
-    }
-      // Send user to next page
-     
-    else  {
-      // Send user to ineligible page
-      res.redirect('/CommodityVolumePage')
-    }
-  
-  },
-
-  router6.post('/commodityvolume', function (req, res) {
-    let count=0;
-    let FRC=new Array();
-    FRC = req.session.data['FRC'];
-    
-    var commodityvolume = req.session.data['commodityvolume'];
-    var AnnualIncome1 = req.session.data['AnnualIncome'];
-    var Descritption=req.session.data['description'];
-     let fromYear=req.session.data['fromYear'];
-     let toYear=req.session.data['toYear'];
-     
-   console.log("commodityvolume:",commodityvolume);
-  
-   
-  
-     
-     if(AnnualIncome1>=500) {
-      for(var i=0;i<commodityvolume.length;i++)
-      {
-      if(commodityvolume[i]>=100)
-      {
-        count++;
-      }
-    }
-    console.log("count",count,commodityvolume.length);
-     if(count==commodityvolume.length)
-     {
-      res.redirect('/checkanswers7')
-     }
-    }
-    else if(AnnualIncome1>=500&&commodityvolume[i]<100) {
-      console.log("enters 1 if");
-      // Send user to ineligible page
-      res.redirect('/checkanswers3')
-    }
-    else if(AnnualIncome1<500)
-    {
-
-      for(var i=0;i<commodityvolume.length;i++)
-      {
-      if(commodityvolume[i]<100)
-      {
-        count++;
-      }
-    }
-    console.log("count",count,commodityvolume.length);
-     if(count==commodityvolume.length)
-     {
-      res.redirect('/checkanswers6')
-     }
-     else{
-      res.redirect('/checkanswers4')
-    }
-     }
+      const config = req.session.data['commodities'].find(_config=>_config.value === commodity)
       
+      return {
+      
+          id: config.value,
+      
+          text: config.text,
+      
+          errorMessage: undefined
+      
+      }
+      
+      })
+     
+   // console.log(" req.session.data['selectedCommodities']", req.session.data['selectedCommodities']);
+    res.redirect('/CommodityVolumePage')
     
-    
-  
-  
+
   },
-  
-  router7.post('/commodityvolume', function (req, res) {
 
-    console.log("commodityvolume:");
-    var commodityvolume = req.session.data['commodityvolume'];
-    console.log("commodityvolume:",commodityvolume);
-    if(commodityvolume<100)
-    {
-      res.redirect('/Exemption');
-    }
-    else{}
-  
-   
-  
-  }
 
-  )
+    router2.post('/AnnualIncome', function (req, res) {
+
+
+      var AnnualIncome = req.session.data['AnnualIncome'];
+      if (AnnualIncome < 500) {
+        console.log("AnnualIncome:", AnnualIncome);
+        res.redirect('/Page_4');
+      }
+      else if (AnnualIncome >= 500) {
+        res.redirect('/CommodityVolumeGetController');
+      }
+
+
+
+    },
+      router4.post('/ParentCompany', function (req, res) {
+
+
+        var ParentCompany = req.session.data['ParentCompany'];
+        var AnnualIncome = req.session.data['AnnualIncome'];
+        //console.log("ParentCompany:", ParentCompany);
+
+        // Check whether the variable matches a condition
+        if (ParentCompany == "No") {
+
+
+          if (AnnualIncome < 500) { res.redirect('/checkanswers') }
+          // Send user to next page
+
+        } else if (ParentCompany == "Yes") {
+          // Send user to ineligible page
+          res.redirect('/ParentCompanyDetails')
+        }
+
+      },
+        router5.post('/ParentCompanyAnnualIncome', function (req, res) {
+
+
+          var ParentCompanyAnnualIncome = req.session.data['ParentCompanyAnnualIncome'];
+
+          console.log("ParentCompanyAnnualIncome:", ParentCompanyAnnualIncome);
+
+          // Check whether the variable matches a condition
+
+
+          if (ParentCompanyAnnualIncome < 500) {
+            res.redirect('/checkanswers1')
+          }
+          // Send user to next page
+
+          else {
+            // Send user to ineligible page
+            res.redirect('/CommodityVolumePage')
+          }
+
+        },
+
+          router6.post('/commodityvolume', function (req, res) {
+            let count = 0;
+            let FRC = new Array();
+            FRC = req.session.data['FRC'];
+
+
+            var commodityvolume = req.session.data['commodityvolume'];
+            var AnnualIncome1 = req.session.data['AnnualIncome'];
+            var Descritption = req.session.data['description'];
+            let fromYear = req.session.data['fromYear'];
+            let toYear = req.session.data['toYear'];
+
+          console.log("req.body:", req.body);
+            
+
+            const selectedCommodities = [...req.session.data['selectedCommodities']];
+
+            const error = req.session.data['errors'].commodities
+            
+            Object.entries(req.body).forEach(([commodity, value])=>{
+              console.log("commodityvalue",commodity, value);
+            
+                const index = selectedCommodities.findIndex(_config=>_config.id === commodity);
+            console.log("index",index);
+            console.log("selectedCommoditiesindex",selectedCommodities[index]);
+               selectedCommodities[index].value = value
+                selectedCommodities[index].errorMessage = value > 200 ? error.invalid : undefined
+            
+            });
+            req.session.data['selectedCommodities'] = selectedCommodities
+            console.log("sc", req.session.data['selectedCommodities']);
+            if(req.session.data['selectedCommodities'].find(commodity=>commodity.errorMessage)){
+
+                  return res.redirect("/CommodityVolumePage")
+              
+              }
+            res.redirect("/Page_7")
+            
+            
+
+
+
+
+            if (AnnualIncome1 >= 500) {
+            
+                res.redirect('/checkanswers7')
+              
+            }
+           
+                res.redirect('/checkanswers4')
+              
+            
+
+
+
+
+
+          },
+
+            
+
+            )
+          )
+        )
+      )
+    )
   )
 )
-)
-  )
-  )
-)
+
