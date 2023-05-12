1d261913986d8359b414670d0b8cf358a119d619
SruthiS-CTS <Sruthi.Sundararajan@cognizant.com>
2023-05-12 16:27:00 +0100

    May12_Dashboard

--- app/routes.js
+++ app/routes.js
@@ -13,11 +13,10 @@ const router5 = govukPrototypeKit.requests.setupRouter()
 const router6 = govukPrototypeKit.requests.setupRouter()
 const router7 = govukPrototypeKit.requests.setupRouter()
 const router8 = govukPrototypeKit.requests.setupRouter()
-const router9=govukPrototypeKit.requests.setupRouter()
+const router9 = govukPrototypeKit.requests.setupRouter()
 const exmap = new Map();
-// Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/Login-answer', function (req, res) {
 
+router.post('/Login-answer', function (req, res) {
 
   var Accountholder = req.session.data['Accountholder'];
   //console.log("Accountholder:", Accountholder);
@@ -32,271 +31,200 @@ router.post('/Login-answer', function (req, res) {
   }
 
 },
+  // router1.post('/FRC', function (req, res) {
 
-  router1.post('/FRC', function (req, res) {
+  //   let FRC = new Array();
 
-    let FRC = new Array();
-   
-    FRC = req.session.data['FRC'];
-    let len = FRC.length;
+  //   FRC = req.session.data['FRC'];
+  //   let len = FRC.length;   
 
+  //   // Check whether the variable matches a condition
+  //   if (FRC == "None of the above") {
+  //     // Send user to next page
+  //     res.redirect('/ConfirmFRC')
+  //   } else {
+  //     // Send user to ineligible page
+  //     res.redirect('/AnnunalTurnover')
+  //   }
 
-   // console.log("FRC:", FRC);
-
-    // Check whether the variable matches a condition
-    if (FRC == "None of the above") {
-      // Send user to next page
-      res.redirect('/ConfirmFRC')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/Page_3')
-    }
-
-  },
-  router1.get('/CommodityVolumeGetController', function (req, res) {
+  // },
+    router1.get('/CommodityVolumeGetController', function (req, res) {
 
 
-    let  fromMonth =req.session.data['fromMonth'];
-    let fromYear=req.session.data['fromYear'];
-    let toMonth= req.session.data['toMonth'];
-    let toYear=req.session.data['toYear'];
+      let fromMonth = req.session.data['fromMonth'];
+      let fromYear = req.session.data['fromYear'];
+      let toMonth = req.session.data['toMonth'];
+      let toYear = req.session.data['toYear'];
 
-    function getMonthName(monthNumber) {
-      const date = new Date();
-      date.setMonth(monthNumber - 1);
-    
-      return date.toLocaleString('en-US', {
-        month: 'long',
-      });
-    }
-  
+      function getMonthName(monthNumber) {
+        const date = new Date();
+        date.setMonth(monthNumber - 1);
 
-   
- req.session.data['FromPeriod']=getMonthName(fromMonth).concat(" ", fromYear);
- req.session.data['ToPeriod']=getMonthName(toMonth).concat(" ", toYear);
- //console.log("req.session.data['FromPeriod']",req.session.data['FromPeriod']);
- //console.log(req.session.data['ToPeriod'],req.session.data['ToPeriod']);
-//  exmap.set(req.session.data['FromPeriod'], req.session.data['Userservice']);
-//  console.log("exmap",exmap);
-//  localStorage.setItem("lastname", "Smith");
-//  document.getElementById("test").innerHTML=localStorage.getItem("lastname");
-
-
-   // {{ data['fromMonth'] }}/   {{ data['fromYear'] }} to  {{ data['toMonth'] }}/{{ data['toYear'] }}
- 
-    
- req.session.data['selectedCommodities']= req.session.data['FRC'].map(commodity=>{
-
-      const config = req.session.data['commodities'].find(_config=>_config.value === commodity)
-      
-      return {
-      
-          id: config.value,
-      
-          text: config.text,
-      
-          errorMessage: undefined
-      
+        return date.toLocaleString('en-US', {
+          month: 'long',
+        });
       }
-      
+
+
+
+      req.session.data['FromPeriod'] = getMonthName(fromMonth).concat(" ", fromYear);
+      req.session.data['ToPeriod'] = getMonthName(toMonth).concat(" ", toYear);
+
+      req.session.data['selectedCommodities'] = req.session.data['FRC'].map(commodity => {
+
+        const config = req.session.data['commodities'].find(_config => _config.value === commodity)
+
+        return {
+
+          id: config.value,
+
+          text: config.text,
+
+          errorMessage: undefined
+
+        }
+
       })
-     
-  // console.log(" req.session.data['selectedCommodities']", req.session.data['selectedCommodities']);
-    res.redirect('/CommodityVolumePage')
-    
-
-  },
-
-
-    router2.post('/AnnualIncome', function (req, res) {
-
-
-      var AnnualIncome = req.session.data['AnnualIncome'];
-      if (AnnualIncome < 500) {
-      //  console.log("AnnualIncome:", AnnualIncome);
-        res.redirect('/Page_4');
-      }
-      else if (AnnualIncome >= 500) {
-        res.redirect('/PeriodOfExemption');
-      }
-
+      res.redirect('/CommodityVolumePage')
 
 
     },
-      router4.post('/ParentCompany', function (req, res) {
 
 
-        var ParentCompany = req.session.data['ParentCompany'];
+      router2.post('/AnnualIncome', function (req, res) {
+
+
         var AnnualIncome = req.session.data['AnnualIncome'];
-        //console.log("ParentCompany:", ParentCompany);
-
-        // Check whether the variable matches a condition
-        if (ParentCompany == "No") {
-
-
-          if (AnnualIncome < 500) { res.redirect('/checkanswers') }
-          // Send user to next page
-
-        } else if (ParentCompany == "Yes") {
-          // Send user to ineligible page
-          res.redirect('/ParentCompanyDetails')
+        if (AnnualIncome < 500) {
+          //  console.log("AnnualIncome:", AnnualIncome);
+          res.redirect('/ParentCompany');
         }
-
+        else if (AnnualIncome >= 500) {
+          res.redirect('/PeriodOfExemption');
+        }
       },
-        router5.post('/ParentCompanyAnnualIncome', function (req, res) {
+        router4.post('/ParentCompany', function (req, res) {
 
 
-          var ParentCompanyAnnualIncome = req.session.data['ParentCompanyAnnualIncome'];
-
-         // console.log("ParentCompanyAnnualIncome:", ParentCompanyAnnualIncome);
-
+          var ParentCompany = req.session.data['ParentCompany'];
+          var AnnualIncome = req.session.data['AnnualIncome'];
+      
           // Check whether the variable matches a condition
+          if (ParentCompany == "No") {
 
 
-          if (ParentCompanyAnnualIncome < 500) {
-            res.redirect('/checkanswers1')
-          }
-          // Send user to next page
+            if (AnnualIncome < 500) { res.redirect('/checkanswers') }
+            // Send user to next page
 
-          else {
+          } else if (ParentCompany == "Yes") {
             // Send user to ineligible page
-            res.redirect('/CommodityVolumePage')
+            res.redirect('/ParentCompanyDetails')
           }
 
         },
-        router7.post('/Userjourney', function (req, res) {
-         
-          //sessionStorage.clear();
-          req.session.data['complete'] = 'No';
-         
-          var Userservice = req.session.data['Userservice'];
-
-         // console.log("req.session.data['period']='January':", req.session.data['period']='January');
-          if(Userservice=="Exemption")
-          {
-            res.redirect("/Page_3")
-
-          }
-          else{
-            res.redirect("/amendCommodities")
-          }
-
-          // Check whether the variable matches a condition
+          router5.post('/ParentCompanyAnnualIncome', function (req, res) {
 
 
-         
+            var ParentCompanyAnnualIncome = req.session.data['ParentCompanyAnnualIncome'];
 
-        },
+            if (ParentCompanyAnnualIncome < 500) {
+              res.redirect('/checkanswers1')
+            }
+            // Send user to next page
 
-          router6.post('/commodityvolume', function (req, res) {
-            // console.log("dates",req.session.data['passport-issued']);
+            else {
+              // Send user to ineligible page
+              res.redirect('/CommodityVolumePage')
+            }
 
-            let count = 0;
-            let FRC = new Array();
-            FRC = req.session.data['FRC'];
+          },
+            router7.post('/Userjourney', function (req, res) {
+              //var Userservice = req.session.data['Userservice'];
+               const {Userservice}=req.session.data;
+               req.session.data={Userservice};
+              //sessionStorage.clear();
+              req.session.data['complete'] = 'No';
 
+             
 
-            var commodityvolume = req.session.data['commodityvolume'];
-            var AnnualIncome1 = req.session.data['AnnualIncome'];
-            var Descritption = req.session.data['description'];
-            let fromYear = req.session.data['fromYear'];
-            let toYear = req.session.data['toYear'];
-            let count1=0;
+              // console.log("req.session.data['period']='January':", req.session.data['period']='January');
+              if (Userservice == "Exemption") {
+                res.redirect("/AnnunalTurnover")
 
-          //console.log("req.body:", req.body);
-            
-
-            const selectedCommodities = [...req.session.data['selectedCommodities']];
-
-            const error = req.session.data['errors'].commodities
-            
-            Object.entries(req.body).forEach(([commodity, value])=>{
-              console.log("commodityvalue",commodity, value);
-            
-                const index = selectedCommodities.findIndex(_config=>_config.id === commodity);
-         //   console.log("index",index);
-           // console.log("selectedCommoditiesindex",selectedCommodities[index]);
-               selectedCommodities[index].value = value
-                selectedCommodities[index].errorMessage = value > 999 ? error.invalid : undefined
-            
-            });
-            req.session.data['selectedCommodities'] = selectedCommodities
-            //console.log("sc", req.session.data['selectedCommodities']);
-            for(let i=0;i<req.session.data['selectedCommodities'].length;i++)
-            {
-              if(selectedCommodities[i].errorMessage!=undefined)
-              {
-              count1++;
               }
-              //console.log("df",count1);
-              
-            }
-            if(count1>0)
-            {
-              return res.redirect("/CommodityVolumePage")
-            }
-            else{
-              res.redirect("/Page_7")
-            }                         
-            
+              else {
+                res.redirect("/amendCommodities")
+              }
 
 
+            },
+
+              router6.post('/commodityvolume', function (req, res) {
+                
+                let count = 0;
+                let FRC = new Array();
+                FRC = req.session.data['FRC'];
 
 
-            
-              
-            
+                var commodityvolume = req.session.data['commodityvolume'];
+                var AnnualIncome1 = req.session.data['AnnualIncome'];
+                var Descritption = req.session.data['description'];
+                let fromYear = req.session.data['fromYear'];
+                let toYear = req.session.data['toYear'];
+                let count1 = 0;
+
+                //console.log("req.body:", req.body);
 
 
+                const selectedCommodities = [...req.session.data['selectedCommodities']];
+
+                const error = req.session.data['errors'].commodities
+
+                Object.entries(req.body).forEach(([commodity, value]) => {
+                  console.log("commodityvalue", commodity, value);
+                  const index = selectedCommodities.findIndex(_config => _config.id === commodity);
+                  selectedCommodities[index].value = value
+                  selectedCommodities[index].errorMessage = value > 999 ? error.invalid : undefined
+
+                });
+                req.session.data['selectedCommodities'] = selectedCommodities
+
+                for (let i = 0; i < req.session.data['selectedCommodities'].length; i++) {
+                  if (selectedCommodities[i].errorMessage != undefined) {
+                    count1++;
+                  }
+
+                }
+                if (count1 > 0) {
+                  return res.redirect("/CommodityVolumePage")
+                }
+                else {
+                  res.redirect("/Page_7")
+                }
+              },
+                router8.post('/appcomplete', function (req, res) {
+                  req.session.data['complete'] = 'Yes';
+                  res.redirect("/applicationcomplete");
+
+                },
+
+                  router9.post('/getdata', function (req, res) {
+                    console.log("req",req.body);
+                    console.log("req.session.data['selectedCommodities']",req.session.data['selectedCommodities'])
+                    
+console.log(" req.session.data['amendedcommodities']", req.session.data['ac'])
+
+                  },
+
+                  )
 
 
-
-          },
-          router8.post('/appcomplete', function (req, res) {
-            req.session.data['complete'] = 'Yes';
-            res.redirect("/applicationcomplete");
-
-            //console.log("APPLICATION COMPLETED",exmap);
-            // req.session.data['exmap'] =exmap
-            // console.log("APPLICATION COMPLETED", req.session.data['exmap']);
-
-          },
-
-          router9.post('/getdata', function (req, res) {
-           
-        
-            var AnnualIncome = req.session.data['AnnualIncome'];
-            var ParentCompany=req.session.data['ParentCompany'];
-  
-            //console.log("ParentCompany:", ParentCompany);
-            if(AnnualIncome<500 && ParentCompany =="Yes")
-            {
-              res.redirect("/checkanswerswithParentCompany")
-  
-            }
-            else if (AnnualIncome<500 && ParentCompany =="No"){
-              res.redirect("/checkanswerswithoutParentCompany")
-            }
-            else if(AnnualIncome>500)
-            {
-              res.redirect("/checkanswers7")
-            }
-  
-            // Check whether the variable matches a condition
-  
-  
-           
-  
-          },
-          )
-
-            
-          )
+                )
+              )
             )
           )
         )
       )
     )
   )
-)
-)
+
